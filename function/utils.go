ed4332a6d29d29520f24c1e7bf394beef4bcf409
airone <erwan.agesne@ynov.com>
2024-05-23 11:51:32 +0200

    merge signup in Authentification

---
+++ function/utils.go
@@ -0,0 +1,36 @@
+package forum
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"regexp"
+	"unicode"
+)
+
+func Encrypt(password string) string {
+	hash := sha256.Sum256([]byte(password))
+	return fmt.Sprintf("%x", hash)
+}
+func VerifyPassword(s string) bool {
+	var hasNumber, hasUpperCase, hasLowercase, hasSpecial bool
+	for _, c := range s {
+		switch {
+		case unicode.IsNumber(c):
+			hasNumber = true
+		case unicode.IsUpper(c):
+			hasUpperCase = true
+		case unicode.IsLower(c):
+			hasLowercase = true
+		case c == '#' || c == '|':
+			return false
+		case unicode.IsPunct(c) || unicode.IsSymbol(c):
+			hasSpecial = true
+		}
+	}
+	return hasNumber && hasUpperCase && hasLowercase && hasSpecial
+}
+
+func EmailValid(e string) bool {
+	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return emailRegex.MatchString(e)
+}
