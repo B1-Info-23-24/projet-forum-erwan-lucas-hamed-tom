9b9d71a1dfea6f124ce182cf3be5bc5903d05f83
Ssnakyx <hamed.kaffa@ynov.com>
2024-05-23 11:40:24 +0200

    made api to add account in the database

--- function/page.go
+++ function/page.go
@@ -1,7 +1,1 @@
 package forum
-
-import "net/http"
-
-func Home(w http.ResponseWriter, r *http.Request, errorMessage string) {
-
-}
--- function/server.go
+++ function/server.go
@@ -11,11 +11,10 @@ import (
 )
 
 func Server() {
-	// Initialize the database
+
 	InitDB()
 	AutoMigrate(DB)
 
-	// Initialize the router for Mux
 	r := mux.NewRouter()
 
 	r.HandleFunc("/login", RenderLoginPageHandler).Methods("GET")
@@ -23,16 +22,13 @@ func Server() {
 	r.HandleFunc("/signup", renderSignupPage).Methods("GET")
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
-	// Start the Mux server in a goroutine
 	go func() {
 		log.Println("Mux server running at http://localhost:8080/")
 		log.Fatal(http.ListenAndServe(":8080", r))
 	}()
 
-	// Initialize the Gin router for the API
 	ginRouter := gin.Default()
 
-	// Configure CORS middleware
 	ginRouter.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:8080"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
@@ -43,13 +39,11 @@ func Server() {
 
 	RegisterRoutes(ginRouter)
 
-	// Start the Gin server
 	go func() {
 		log.Println("Gin server running at http://localhost:8081/api/")
 		log.Fatal(ginRouter.Run(":8081"))
 	}()
 
-	// Prevent the main function from exiting
 	select {}
 }
 
