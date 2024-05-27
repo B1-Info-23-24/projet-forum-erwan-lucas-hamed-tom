bc4dd2401dab9fca89d890ec51c2a68b489340c2
airone <erwan.agesne@ynov.com>
2024-05-27 09:24:24 +0200

    change botton fermer in X

--- function/Test.go
+++ function/Test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./index.html", "./templates/header.html", "./templates/menu.html", "./pages/testSignup.html", "./pages/testLogin.html")
+	template, err := template.ParseFiles("./index.html", "./templates/header.html", "./templates/menu.html", "./templates/Signup.html", "./templates/Login.html")
 	if err != nil {
 		log.Fatal(err)
 	}
